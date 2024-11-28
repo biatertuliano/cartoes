35574ae41bdb599fe6a55810683956db359035ac
biatertuliano <biatertuliano866@gmail.com>
2024-11-27 22:40:27 -0300

    upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,35 @@
 criaCartao(
-    "Programação",
-    "O que é JavaScript?",
-    "O JavaScript é uma linguagem de programação"
+    "Marvel",
+    "Em que ano foi lançado Vingadores Ultimato?",
+    "Vingadores Ultimato, filme da marvel, teve seu lançamento no ano de 2019 com 3h 1m de duração com a impressionante bilheteria totalizando US$ 2,797 bilhões."
 )
 
 criaCartao(
-    "Geografia",
-    "Qual a capital da França?",
-    "A capital da frança é Paris."
+    "DC Comics",
+    "Qual a Velocidade do FLASH(Barry Allen)?",
+    "A velocidade do Flash é estimada em torno de 1100 Km/h, ou até mesmo aproximadamente 305,5 m/s, sendo capaz de ultrapassar a velocidade da luz."
 )
 
 criaCartao(
-    "Programação",
-    "O que é uma função?",
-    "Uma função é o bloco de código que executa alguma tarefa."
+    "Adam Sandler",
+    "Quais são os atores principais do filme Gente Grande?",
+    "O Elenco principal conta com grandes atores, dentre eles: Adam Sandler, Kevin James, David Spade, Chris Rock e Rob Schneider."
 )
 
 criaCartao(
-    "Programação",
-    "O que é uma função?",
-    "Uma função é o bloco de código que executa alguma tarefa."
+    "Oscar",
+    "Qual filme mais visto do mundo?",
+    "O filme mais visto do mundo é Avatar(2009), que arrecadou quase quase US$ 3 bilhões."
+)
+
+criaCartao(
+    "Assustador",
+    "Qual é o filme considerado o mais assustador?",
+    "De acordo com um estudo. o filme mais assustador de todos os tempos é A Entidade (2012)."
+)
+
+criaCartao(
+    "Cinema",
+    "Quem inventou o Cinema?",
+    "Os irmãos franceses Auguste e Louis Lumière são geralmente considerados os criadores do cinema, pois foram os primeiros a projetar um filme em público."
 )
