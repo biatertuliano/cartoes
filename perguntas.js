01713967e8c83d952bf42895ccb558ee3e8c7d23
biatertuliano <biatertuliano866@gmail.com>
2024-11-27 22:02:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "Programação",
+    "O que é JavaScript?",
+    "O JavaScript é uma linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da frança é Paris."
+)
+
+criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é o bloco de código que executa alguma tarefa."
+)
+
+criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é o bloco de código que executa alguma tarefa."
+)
